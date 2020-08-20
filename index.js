6f1c90b65d3c9a2122cb84781ce56f55ef2c28d7
pj0702 <pjcastit@pronaca.com>
2020-08-20 15:50:29 -0500

    Arreglando errores

--- index.js
+++ index.js
@@ -33,33 +33,30 @@ const User = sequelize.define('user', {
 User.sync({ force: true }) // Now the `users` table in the database corresponds to the model definition
 router.get('/', (req, res) => res.json({ message: 'Hello World' }))
 router.post('/createUser', jsonParser, async(req, res) => {
-        try {
-            console.log('data:', req.body)
-            const newUser = new User(req.body)
-            await newUser.save()
-            res.json({ user: newUser }) // Returns the new user that is created in the database
-        } catch (error) {
-            console.error(error)
-            res.end("error");
-        }
-    }) <<
-    << << < HEAD
-app.listen(port, () => console.log(`Ejecutando api en puerto ${port}`)) ===
-    === =
-    router.get('/user/:userId', async(req, res) => {
-        const userId = req.params.userId
-        try {
-            const user = await User.findAll({
-                where: {
-                    id: userId
-                }
-            })
-            res.json({ user })
-        } catch (error) {
-            console.error(error)
-            res.end("error");
-        }
-    })
+    try {
+        console.log('data:', req.body)
+        const newUser = new User(req.body)
+        await newUser.save()
+        res.json({ user: newUser }) // Returns the new user that is created in the database
+    } catch (error) {
+        console.error(error)
+        res.end("error");
+    }
+})
+router.get('/user/:userId', async(req, res) => {
+    const userId = req.params.userId
+    try {
+        const user = await User.findAll({
+            where: {
+                id: userId
+            }
+        })
+        res.json({ user })
+    } catch (error) {
+        console.error(error)
+        res.end("error");
+    }
+})
 router.get('/users', async(req, res) => {
     const userId = req.params.userId
     try {
@@ -71,5 +68,8 @@ router.get('/users', async(req, res) => {
     }
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`)) >>>
-    >>> > version0 .0 .1
+function getRequestString(userId) {
+    return "user"
+}
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
